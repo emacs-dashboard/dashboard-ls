284db01b4fee917c85cb2c987f2c8df6bf33927e
JenChieh <jcs090218@gmail.com>
2021-01-02 17:05:35 +0800

    Fixed messing working path.

--- dashboard-ls.el
+++ dashboard-ls.el
@@ -45,11 +45,19 @@
   "Update to date current path.
 Use this variable when you don't have the `default-directory' up to date.")
 
+(defvar dashboard-ls--record-path nil
+  "Record of the current working directory.")
+
+(defun dashboard-ls--current-path ()
+  "Return the current path the user is on."
+  (setq dashboard-ls--record-path (or dashboard-ls-path default-directory))
+  dashboard-ls--record-path)
+
 (defun dashboard-ls--insert-dir (list-size)
   "Add the list of LIST-SIZE items from current directory."
   (dashboard-insert-section
    "Current Directories:"
-   (let* ((current-dir (or dashboard-ls-path default-directory))
+   (let* ((current-dir (dashboard-ls--current-path))
           (dir-lst (when (f-dir-p current-dir) (f-directories current-dir)))
           (opt-dir-lst '()))
      (dolist (dir dir-lst)
@@ -59,14 +67,15 @@ Use this variable when you don't have the `default-directory' up to date.")
      (reverse opt-dir-lst))
    list-size
    (dashboard-get-shortcut 'ls-directories)
-   `(lambda (&rest ignore) (find-file-existing ,el))
+   `(lambda (&rest ignore)
+      (find-file-existing (concat dashboard-ls--record-path ,el)))
    (abbreviate-file-name el)))
 
 (defun dashboard-ls--insert-file (list-size)
   "Add the list of LIST-SIZE items from current files."
   (dashboard-insert-section
    "Current Files:"
-   (let* ((current-dir (or dashboard-ls-path default-directory))
+   (let* ((current-dir (dashboard-ls--current-path))
           (file-lst (when (f-dir-p current-dir) (f-files current-dir)))
           (opt-file-lst '()))
      (dolist (file file-lst)
@@ -76,7 +85,8 @@ Use this variable when you don't have the `default-directory' up to date.")
      (reverse opt-file-lst))
    list-size
    (dashboard-get-shortcut 'ls-files)
-   `(lambda (&rest ignore) (find-file-existing ,el))
+   `(lambda (&rest ignore)
+      (find-file-existing (concat dashboard-ls--record-path ,el)))
    (abbreviate-file-name el)))
 
 (provide 'dashboard-ls)
